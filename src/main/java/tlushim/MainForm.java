ce0b40fbfb4f7efb38640fab5180445d4f428ab9
gchaim <gchaimke@gmail.com>
2020-02-13 15:46:27 +0200

    add task

--- src/main/java/tlushim/MainForm.java
+++ src/main/java/tlushim/MainForm.java
@@ -44,6 +44,8 @@ public class MainForm extends javax.swing.JFrame {
         lessHours = new javax.swing.JLabel();
         totalHours = new javax.swing.JLabel();
         jProgressBar1 = new javax.swing.JProgressBar();
+        jLabel3 = new javax.swing.JLabel();
+        lblStatus = new javax.swing.JLabel();
         spMonth = new javax.swing.JScrollPane();
         tbMonth = new javax.swing.JTable();
 
@@ -52,6 +54,7 @@ public class MainForm extends javax.swing.JFrame {
         setMinimumSize(new java.awt.Dimension(500, 500));
 
         pLogin.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "כניסה ל- tlushim.co.il", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Arial", 1, 14))); // NOI18N
+        pLogin.setMaximumSize(new java.awt.Dimension(200, 500));
 
         jLabel1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -114,6 +117,13 @@ public class MainForm extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
+        jLabel3.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("Status");
+
+        lblStatus.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblStatus.setMaximumSize(new java.awt.Dimension(100, 100));
+
         javax.swing.GroupLayout pLoginLayout = new javax.swing.GroupLayout(pLogin);
         pLogin.setLayout(pLoginLayout);
         pLoginLayout.setHorizontalGroup(
@@ -125,6 +135,8 @@ public class MainForm extends javax.swing.JFrame {
             .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addComponent(totalPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addComponent(jProgressBar1, javax.swing.GroupLayout.DEFAULT_SIZE, 161, Short.MAX_VALUE)
+            .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(lblStatus, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         pLoginLayout.setVerticalGroup(
             pLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -139,10 +151,14 @@ public class MainForm extends javax.swing.JFrame {
                 .addComponent(pfPass, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(btnLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(30, 30, 30)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
-                .addComponent(totalPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(totalPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblStatus, javax.swing.GroupLayout.DEFAULT_SIZE, 65, Short.MAX_VALUE))
         );
 
         tbMonth.setAutoCreateRowSorter(true);
@@ -197,9 +213,9 @@ public class MainForm extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(spMonth, javax.swing.GroupLayout.Alignment.TRAILING)
+            .addComponent(spMonth, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 509, Short.MAX_VALUE)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(15, Short.MAX_VALUE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(pLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
@@ -213,14 +229,20 @@ public class MainForm extends javax.swing.JFrame {
         HtmlParser parse = new HtmlParser(tfUser.getText(),pfPass.getText(),site);
         ExecutorService executor = Executors.newFixedThreadPool(5);
         
-        // Callable, return a future, submit and run the task async
-        Future<String> futureTask1 = executor.submit(() -> {
-            data = "test";
-            String data = parse.getData();
-            System.out.println("I'm Callable task.");
-            moreHours.setText("ok");
-            jProgressBar1.setIndeterminate(false);
-            return data;
+        // Runnable, return void, submit and run the task async
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Start parser Task");
+                data = "test";
+                String data = parse.getData();
+                if(data.equals("error")|| data.isEmpty()){
+                    lblStatus.setText("Error, check internet status or user name or password.");
+                }else{
+                    lblStatus.setText("ok");
+                }
+                jProgressBar1.setIndeterminate(false);
+            }
         });
         
         //String data = parse.getData();
@@ -265,7 +287,9 @@ public class MainForm extends javax.swing.JFrame {
     private javax.swing.JButton btnLogin;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JProgressBar jProgressBar1;
+    private javax.swing.JLabel lblStatus;
     private javax.swing.JLabel lessHours;
     private javax.swing.JLabel moreHours;
     private javax.swing.JPanel pLogin;
